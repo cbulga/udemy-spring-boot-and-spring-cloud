49b4591ee5fc6cc9bfbabcd1b6603b61f66ad4e6
Cristian Bulgarelli <cristian.bulgarelli@abstract.it>
2022-01-31 15:22:08 +0100

    Sezione 10 - Protegger il Config Server (lezione 100 completata)

---
+++ config-server/src/main/java/com/xantrix/webapp/configserver/BasicSecurityConfiguration.java
@@ -0,0 +1,48 @@
+package com.xantrix.webapp.configserver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class BasicSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Value("${sicurezza.userpwd}")
+    String user_password;
+
+    @Value("${sicurezza.adminpwd}")
+    String admin_password;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(new BCryptPasswordEncoder().encode(user_password))
+                .roles("USER")
+            .and()
+                .withUser("admin")
+                .password(new BCryptPasswordEncoder().encode(admin_password))
+                .roles("USER", "ACTUATOR");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .httpBasic()
+            .and()
+                .authorizeRequests()
+                .antMatchers("/actuator/**").hasAuthority("ROLE_ACTUATOR")
+                .antMatchers("/**").hasAuthority("ROLE_USER");
+    }
+}
