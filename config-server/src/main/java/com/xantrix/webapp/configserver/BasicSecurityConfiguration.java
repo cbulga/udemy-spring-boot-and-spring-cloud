b99b0807e7f32b81a007a37a54eeb0e9ff5f42cf
Cristian Bulgarelli <cristian.bulgarelli@abstract.it>
2022-01-31 16:51:52 +0100

    Sezione 10 - Usare le variabili d'ambiente per memorizzare le password (lezione 101 complatata)

--- config-server/src/main/java/com/xantrix/webapp/configserver/BasicSecurityConfiguration.java
+++ config-server/src/main/java/com/xantrix/webapp/configserver/BasicSecurityConfiguration.java
@@ -1,5 +1,7 @@
 package com.xantrix.webapp.configserver;
 
+import lombok.extern.slf4j.Slf4j;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -10,13 +12,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@Slf4j
 public class BasicSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Value("${sicurezza.userpwd}")
+    @Value("${userpwd}")
     String user_password;
 
-    @Value("${sicurezza.adminpwd}")
+    @Value("${adminpwd}")
     String admin_password;
+
+    @Value("${jasypt.encryptor.password}")
+    String password;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -25,13 +31,15 @@ public class BasicSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        log.debug("userpwd={}", this.user_password);
+        log.debug("adminpwd={}", this.admin_password);
         auth.inMemoryAuthentication()
                 .withUser("user")
-                .password(new BCryptPasswordEncoder().encode(user_password))
+                .password(new BCryptPasswordEncoder().encode(decryptString(user_password)))
                 .roles("USER")
             .and()
                 .withUser("admin")
-                .password(new BCryptPasswordEncoder().encode(admin_password))
+                .password(new BCryptPasswordEncoder().encode(decryptString(admin_password)))
                 .roles("USER", "ACTUATOR");
     }
 
@@ -44,5 +52,17 @@ public class BasicSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/actuator/**").hasAuthority("ROLE_ACTUATOR")
                 .antMatchers("/**").hasAuthority("ROLE_USER");
+    }
+
+    protected String decryptString(String passwordToDecrypt) {
+        StandardPBEStringEncryptor decryptor = new StandardPBEStringEncryptor();
+        decryptor.setPassword(this.password);
+        return decryptor.decrypt(passwordToDecrypt);
+    }
+
+    protected String encrypt(String stringToEncrypt) {
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setPassword(this.password);
+        return encryptor.encrypt(stringToEncrypt);
     }
 }
--- config-server/src/test/java/com/xantrix/webapp/configserver/ApplicationTests.java
+++ config-server/src/test/java/com/xantrix/webapp/configserver/ApplicationTests.java
@@ -1,13 +1,53 @@
 package com.xantrix.webapp.configserver;
 
+import lombok.extern.slf4j.Slf4j;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+//@SpringBootTest(properties = {"userpwd=" + ApplicationTests.USER_PASSWORD, "adminpwd=MagicaBula_2018", "jasypt.encryptor.password=amalaPazzaInterAmala"})
+@Slf4j
 class ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    /**
+     * userpwd=BimBumBam_2018 => encrypted value=kSnZ/ISHShFEPeok00UCYUqFSUu3P+co
+     * adminpwd=MagicaBula_2018 => encrypted value=1QhYQyk24MvDG9X/UetDKIChLEREECij
+     */
+    public static final String JASYPT_PASSWORD_TO_ENCRYPT = "amalaPazzaInterAmala";
+    public static final String ENCRYPTED_JASYPT_PASSWORD_TO_DECRYPT = "amalaPazzaInterAmala";
+    public static final String USER_PASSWORD = "BimBumBam_2018";
+    public static final String ADMIN_PASSWORD = "MagicaBula_2018";
+//    @Autowired
+//    private BasicSecurityConfiguration securityConfiguration;
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//    @Test
+//    void encryptPassword() {
+//        String encryptedPassword = securityConfiguration.encrypt(JASYPT_PASSWORD_TO_ENCRYPT);
+//        log.debug("password to encrypt {} = {}", JASYPT_PASSWORD_TO_ENCRYPT, encryptedPassword);
+//        String decryptedPassword = securityConfiguration.decryptString(encryptedPassword);
+//        log.debug("decrypted password: {}", decryptedPassword);
+//        assertThat(JASYPT_PASSWORD_TO_ENCRYPT).isEqualTo(decryptedPassword);
+//        log.debug("userpwd=" + USER_PASSWORD + " => encrypted value={}", securityConfiguration.encrypt(USER_PASSWORD));
+//        log.debug("adminpwd=" + ADMIN_PASSWORD + " => encrypted value={}", securityConfiguration.encrypt(ADMIN_PASSWORD));
+//    }
 
+    @Test
+    void encryptPasswordInternal() {
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setPassword(JASYPT_PASSWORD_TO_ENCRYPT);
+        String encryptedPassword = encryptor.encrypt(JASYPT_PASSWORD_TO_ENCRYPT);
+        log.debug("password to encrypt {} = {}", JASYPT_PASSWORD_TO_ENCRYPT, encryptedPassword);
+        String decryptedPassword = encryptor.decrypt(encryptedPassword);
+        log.debug("decrypted password: {}", decryptedPassword);
+        assertThat(JASYPT_PASSWORD_TO_ENCRYPT).isEqualTo(decryptedPassword);
+        log.debug("userpwd=" + USER_PASSWORD + " => encrypted value={}", encryptor.encrypt(USER_PASSWORD));
+        log.debug("adminpwd=" + ADMIN_PASSWORD + " => encrypted value={}", encryptor.encrypt(ADMIN_PASSWORD));
+    }
 }
