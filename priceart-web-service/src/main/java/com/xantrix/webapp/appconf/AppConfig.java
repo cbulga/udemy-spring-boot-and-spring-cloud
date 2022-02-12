38db547bd811e5eef3419f82c65925b8d68d5391
cbulga <cbulga@yahoo.it>
2022-02-12 16:01:05 +0100

    Sezione 11: Introduzione allo Spring Cloud Bus (lezione 105 completata, introduzione rabbit MQ)

--- priceart-web-service/src/main/java/com/xantrix/webapp/appconf/AppConfig.java
+++ priceart-web-service/src/main/java/com/xantrix/webapp/appconf/AppConfig.java
@@ -1,28 +1,14 @@
 package com.xantrix.webapp.appconf;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties("application")
+@Data
 public class AppConfig {
+
     private String listino;
-
-    public Double getSconto() {
-        return sconto;
-    }
-
-    public void setSconto(Double sconto) {
-        this.sconto = sconto;
-    }
-
     private Double sconto = 0.00;
-
-    public String getListino() {
-        return listino;
-    }
-
-    public void setListino(String Listino) {
-        this.listino = Listino;
-    }
 }
