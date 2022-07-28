305efa144723dee600e74e15433130f5d13bb7e0
Cristian Bulgarelli <cbulga@yahoo.it>
2022-07-28 08:17:19 +0200

    Sezione 7 - 40 Gestione della Cache in Spring Boot - Parte 2 - aggiornamento gestione cache

--- articoli-web-service/src/main/java/com/xantrix/webapp/service/ArticoliServiceImpl.java
+++ articoli-web-service/src/main/java/com/xantrix/webapp/service/ArticoliServiceImpl.java
@@ -2,9 +2,12 @@ package com.xantrix.webapp.service;
 
 import com.xantrix.webapp.dtos.ArticoliDto;
 import com.xantrix.webapp.entities.Articoli;
+import com.xantrix.webapp.entities.Barcode;
 import com.xantrix.webapp.repository.ArticoliRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
@@ -14,20 +17,22 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
-@CacheConfig(cacheNames = "articoli")
+@CacheConfig(cacheNames = {"articoli"})
+@Slf4j
 public class ArticoliServiceImpl implements ArticoliService {
 
     @Autowired
     private ArticoliRepository articoliRepository;
     @Autowired
     private ModelMapper modelMapper;
+
+    @Autowired
+    private CacheManager cacheManager;
 
     @Override
     @Cacheable
@@ -52,7 +57,7 @@ public class ArticoliServiceImpl implements ArticoliService {
     @Override
     @Cacheable(value = "articolo", key = "#codArt", sync = true)
     public ArticoliDto selByCodArt(String codArt) {
-        Articoli articoli = articoliRepository.findByCodArt(codArt);
+        Articoli articoli = selByCodArt2(codArt);
         return articoli != null ? modelMapper.map(articoli, ArticoliDto.class) : null;
     }
 
@@ -62,9 +67,9 @@ public class ArticoliServiceImpl implements ArticoliService {
     }
 
     @Override
-    @Cacheable
-    public ArticoliDto selByBarCode(String barCode) {
-        Articoli articoli = articoliRepository.selByEan(barCode);
+    @Cacheable(value = "barcode", key = "#barcode", sync = true)
+    public ArticoliDto selByBarCode(String barcode) {
+        Articoli articoli = articoliRepository.selByEan(barcode);
         return articoli != null ? modelMapper.map(articoli, ArticoliDto.class) : null;
     }
 
@@ -73,11 +78,14 @@ public class ArticoliServiceImpl implements ArticoliService {
     @Caching(evict = {
             // tutti i metodi che usano @Cacheable senza parametri avranno la cache svuotata
             @CacheEvict(cacheNames = "articoli", allEntries = true),
+            // tutti i metodi che usano @Cacheable con key = "#barcode" avranno la cache svuotata
+//            @CacheEvict(cacheNames = "barcode", key = "#articolo.barcode[0].barcode"),
             // tutti i metodi che usano @Cacheable con key = "#codArt" avranno la cache svuotata
             @CacheEvict(cacheNames = "articolo", key = "#articolo.codArt")
     })
     public void delArticolo(Articoli articolo) {
         articoliRepository.delete(articolo);
+        this.evictCache(articolo.getBarcode());
     }
 
     @Override
@@ -85,10 +93,29 @@ public class ArticoliServiceImpl implements ArticoliService {
     @Caching(evict = {
             // tutti i metodi che usano @Cacheable senza parametri avranno la cache svuotata
             @CacheEvict(cacheNames = "articoli", allEntries = true),
-            // tutti i metodi che usano @Cacheable con key = "#codArt" avranno la cache svuotata
+            // tutti i metodi che usano @Cacheable con key = "#barcode" avranno la cache svuotata per quel barcode
+//            @CacheEvict(cacheNames = "barcode", key = "#articolo.barcode[0].barcode"),
+            // tutti i metodi che usano @Cacheable con key = "#codArt" avranno la cache svuotata per quell'articolo
             @CacheEvict(cacheNames = "articolo", key = "#articolo.codArt")
     })
     public void insArticolo(Articoli articolo) {
         articoliRepository.save(articolo);
+        this.evictCache(articolo.getBarcode());
+    }
+
+    private void evictCache(Set<Barcode> ean) {
+        ean.forEach((Barcode barcode) -> {
+            log.info("Eliminazione cache barcode " + barcode.getBarcode());
+
+            cacheManager.getCache("barcode").evict(barcode.getBarcode());
+        });
+    }
+
+    @Override
+    public void cleanCaches() {
+        cacheManager.getCacheNames().forEach(item -> {
+            log.info(String.format("Eliminazione cache %s", item));
+            cacheManager.getCache(item).clear();
+        });
     }
 }
