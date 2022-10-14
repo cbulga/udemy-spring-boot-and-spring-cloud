c0a8dcd3bcf1c26b40dccd73cadd8f2ce19e344a
Cristian Bulgarelli <cbulga@yahoo.it>
2022-10-14 08:23:21 +0200

    Sezione 15 - 123. Creazione dell'Eureka Server

---
+++ eurekasrv/src/test/java/com/xantrix/webapp/eurekasrv/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xantrix.webapp.eurekasrv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
