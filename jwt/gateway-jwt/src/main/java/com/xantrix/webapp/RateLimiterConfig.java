f87ad93a56f468a3bfd9cd85681fb378bf6afc5e
Cristian Bulgarelli <cbulga@yahoo.it>
2022-11-20 10:37:18 +0100

    Sezione 18 - 148. (New) Rate Limiting Con Redis

---
+++ jwt/gateway-jwt/src/main/java/com/xantrix/webapp/RateLimiterConfig.java
@@ -0,0 +1,15 @@
+package com.xantrix.webapp;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class RateLimiterConfig {
+
+    @Bean
+    KeyResolver userKeyResolver() {
+        return exchange -> Mono.just("1");
+    }
+}
--- jwt/gateway-jwt/src/main/java/com/xantrix/webapp/filter/JwtAuthenticationFilter.java
+++ jwt/gateway-jwt/src/main/java/com/xantrix/webapp/filter/JwtAuthenticationFilter.java
@@ -1,12 +1,11 @@
 package com.xantrix.webapp.filter;
 
-
 import com.xantrix.webapp.exception.JwtTokenMalformedException;
 import com.xantrix.webapp.exception.JwtTokenMissingException;
 import com.xantrix.webapp.util.JwtUtil;
 import lombok.extern.java.Log;
-import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
@@ -19,8 +18,28 @@ import java.util.function.Predicate;
 
 @Component
 @Log
-public class JwtAuthenticationFilter implements GatewayFilter {
+public class JwtAuthenticationFilter implements GlobalFilter {
 
+    private static final List<String> ADMIN_ENDPOINTS = List.of("/api/prezzi/elimina",
+            "/api/listino/inserisci",
+            "/api/listino/elimina",
+            "/api/articoli/inserisci",
+            "/api/articoli/modifica",
+            "/api/articoli/elimina",
+            "/api/prezzi/elimina",
+            "/api/listino/inserisci",
+            "/api/listino/elimina",
+            "/actuator/gateway/**");
+    private static final List<String> USER_ENDPOINTS = List.of("/api/articoli/cerca/ean",
+            "/api/articoli/cerca/codice",
+            "/api/articoli/cerca/descrizione",
+            "/api/prezzi",
+            "/api/prezzi/info",
+            "/api/listino/cerca/id");
+    private static final Predicate<ServerHttpRequest> IS_ADMIN_ENDPOINT = r -> ADMIN_ENDPOINTS.stream()
+            .noneMatch(uri -> r.getURI().getPath().contains(uri));
+    private static final Predicate<ServerHttpRequest> IS_USER_ENDPOINT = r -> USER_ENDPOINTS.stream()
+            .noneMatch(uri -> r.getURI().getPath().contains(uri));
     private final JwtUtil jwtUtil;
 
     public JwtAuthenticationFilter(JwtUtil jwtUtil) {
@@ -31,50 +50,42 @@ public class JwtAuthenticationFilter implements GatewayFilter {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
 
-        final List<String> adminEndpoints = List.of("/api/prezzi/elimina",
-                "/api/listino/inserisci",
-                "/api/listino/elimina",
-                "/api/articoli/inserisci",
-                "/api/articoli/modifica",
-                "/api/articoli/elimina",
-                "/actuator/gateway/**");
+        boolean isAdmin = !IS_ADMIN_ENDPOINT.test(request);
+        boolean isUser = !IS_USER_ENDPOINT.test(request);
 
-        Predicate<ServerHttpRequest> isAdminEndPoints = r -> adminEndpoints.stream()
-                .noneMatch(uri -> r.getURI().getPath().contains(uri));
-
-        boolean isAdmin = !isAdminEndPoints.test(request);
-
-        if (!request.getHeaders().containsKey("Authorization")) {
+        if ((isAdmin || isUser) && !request.getHeaders().containsKey("Authorization")) {
             ServerHttpResponse response = exchange.getResponse();
             response.setStatusCode(HttpStatus.UNAUTHORIZED);
             return response.setComplete();
         }
 
-        final String token = request.getHeaders().getOrEmpty("Authorization").get(0).replace("Bearer ", "");
+        if (isUser || isAdmin) {
+            final String token = request.getHeaders().getOrEmpty("Authorization").get(0).replace("Bearer ", "");
 
-        try {
-            jwtUtil.validateToken(token);
-        } catch (JwtTokenMalformedException | JwtTokenMissingException e) {
-            ServerHttpResponse response = exchange.getResponse();
-            response.setStatusCode(HttpStatus.BAD_REQUEST);
-            return response.setComplete();
+            try {
+                jwtUtil.validateToken(token);
+            } catch (JwtTokenMalformedException | JwtTokenMissingException e) {
+                ServerHttpResponse response = exchange.getResponse();
+                response.setStatusCode(HttpStatus.BAD_REQUEST);
+                return response.setComplete();
+            }
+
+            String user = jwtUtil.getUsernameFromToken(token);
+            log.info(user);
+
+            List<String> authorities = jwtUtil.getAuthFromToken(token);
+
+            if (isAdmin && !authorities.contains("ROLE_ADMIN")
+                    || isUser && !authorities.contains("ROLE_USER")) {
+                ServerHttpResponse response = exchange.getResponse();
+                response.setStatusCode(HttpStatus.FORBIDDEN);
+                return response.setComplete();
+            }
+
+            /*
+            exchange.getRequest().mutate().header("id", String.valueOf(claims.get("id"))).build();
+            */
         }
-
-        String user = jwtUtil.getUsernameFromToken(token);
-        log.info(user);
-
-        List<String> authorities = jwtUtil.getAuthFromToken(token);
-
-        if (isAdmin && !authorities.contains("ROLE_ADMIN")) {
-            ServerHttpResponse response = exchange.getResponse();
-            response.setStatusCode(HttpStatus.FORBIDDEN);
-
-            return response.setComplete();
-        }
-
-		/*
-		exchange.getRequest().mutate().header("id", String.valueOf(claims.get("id"))).build();
-		*/
 
         return chain.filter(exchange);
     }
